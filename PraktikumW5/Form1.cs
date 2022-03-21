787f89a0f40cb130954cd2a80d1317fe6b704897
Michell Christanto <mchristanto01@student.ciputra.ac.id>
2022-03-21 08:52:22 +0700

    95%

--- PraktikumW5/Form1.cs
+++ PraktikumW5/Form1.cs
@@ -9,21 +9,30 @@ namespace PraktikumW5
        
         private void FormMateri_Load(object sender, EventArgs e)
         {
-            ListBox listBoxKoleksi = new ListBox();
-            this.Controls.Add(listBoxKoleksi);
+            
+            rButtonRed.Checked = true;
         }
 
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
-            tBoxData.Text = listBoxKoleksi.Text;
-            lblOutput.Text = listBoxKoleksi.SelectedIndex.ToString();
+         
+            if (listBoxKoleksi.Items.Contains(tBoxData.Text))
+            {
+                MessageBox.Show("Input Kembar");
+            }
+            else
+            {
+                listBoxKoleksi.Items.Add(tBoxData.Text);
+            }
 
         }
        
         private void listBoxKoleksi_SelectedIndexChanged(object sender, EventArgs e)
         {
+            lblOutput.Text = listBoxKoleksi.SelectedItem.ToString();
             
+
         }
 
         private void checkBoxAktif_CheckedChanged(object sender, EventArgs e)
@@ -39,7 +48,7 @@ namespace PraktikumW5
                     lblOutput.ForeColor = Color.Red;
                 }
             }
-            else if (checkBoxAktif.CheckState == CheckState.Indeterminate)
+            else if (checkBoxAktif.CheckState == CheckState.Unchecked)
             {
                 lblOutput.ForeColor = Color.Black;
             }
@@ -47,27 +56,58 @@ namespace PraktikumW5
 
         private void rButtonRed_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBoxAktif.CheckState == CheckState.Checked)
+            {
+                if (rButtonBlue.Checked == true)
+                {
+                    lblOutput.ForeColor = Color.Blue;
+                }
+                else if (rButtonRed.Checked == true)
+                {
+                    lblOutput.ForeColor = Color.Red;
+                }
+            }
+            else if (checkBoxAktif.CheckState == CheckState.Unchecked)
+            {
+                lblOutput.ForeColor = Color.Black;
+            }
         }
 
         private void rButtonBlue_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBoxAktif.CheckState == CheckState.Checked)
+            {
+                if (rButtonBlue.Checked == true)
+                {
+                    lblOutput.ForeColor = Color.Blue;
+                }
+                else if (rButtonRed.Checked == true)
+                {
+                    lblOutput.ForeColor = Color.Red;
+                }
+            }
+            else if (checkBoxAktif.CheckState == CheckState.Unchecked)
+            {
+                lblOutput.ForeColor = Color.Black;
+            }
         }
 
         private void lblOutput_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            listBoxKoleksi.Controls.Clear();
+            listBoxKoleksi.Items.Clear();
+            tBoxData.Clear();
+            
+
         }
 
         private void tBoxData_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
